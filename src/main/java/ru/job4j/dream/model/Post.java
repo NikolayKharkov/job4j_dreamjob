fd449e8d27cd771fe1bd8a82cb3aa4a48a81dc7e
NikolayKharkov <vtiko564@gmail.com>
2022-02-08 14:47:57 +0300

    4. candicates.jsp - список кандидатов. [#276522]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,62 @@
+package ru.job4j.dream.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate created;
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public Post(int id, String name, String description, String created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = LocalDate.parse(created, FORMATTER);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,44 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Store {
+
+    private static final Store INST = new Store();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job",
+                "Description for Junior Vacation",
+                "2021-04-12"));
+        posts.put(2, new Post(2, "Middle Java Job",
+                "Description for Middle Vacation",
+                "2021-09-28"));
+        posts.put(3, new Post(3, "Senior Java Job",
+                "Description for Senior Vacation",
+                "2021-09-28"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
+    }
+}
