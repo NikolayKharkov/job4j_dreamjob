2f4696c9502124690c183d2c12a022a0730f6b30
NikolayKharkov <vtiko564@gmail.com>
2022-02-11 18:55:22 +0300

    1. Загрузка и скачивание файла. [#154183 #263099]

---
+++ src/main/java/ru/job4j/dream/store/MemStore.java
@@ -0,0 +1,76 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MemStore implements Store {
+
+    private static final MemStore INST = new MemStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private static final AtomicInteger POST_ID = new AtomicInteger(4);
+
+    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
+    private MemStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "Description for Junior Vacation"));
+        posts.put(2, new Post(2, "Middle Java Job", "Description for Middle Vacation"));
+        posts.put(3, new Post(3, "Senior Java Job", "Description for Senior Vacation"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
+    }
+
+    public static MemStore instOf() {
+        return INST;
+    }
+
+    @Override
+    public Collection<Post> findAllPosts() {
+        return posts.values();
+    }
+
+    @Override
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
+    }
+
+    @Override
+    public void savePost(Post post) {
+        if (post.getId() == 0) {
+            post.setId(POST_ID.incrementAndGet());
+        }
+        posts.put(post.getId(), post);
+    }
+
+    @Override
+    public Post findPostById(int id) {
+        return posts.get(id);
+    }
+
+    @Override
+    public Candidate findCandidateById(int id) {
+        return candidates.get(id);
+    }
+
+    @Override
+    public void saveCandidate(Candidate candidate) {
+        if (candidate.getId() == 0) {
+            candidate.setId(CANDIDATE_ID.incrementAndGet());
+        }
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    @Override
+    public void deleteCandidate(int id) {
+        candidates.remove(id);
+    }
+}
